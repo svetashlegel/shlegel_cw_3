fabd518ba0e6f82b8e9e5a4726e400ce3988adab
shlegel <sveta.shlegel@mail.ru>
2023-04-29 11:32:10 +0300

    added file with operations and file utils.py

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def load_operations(path):
+    """Загружает список операций из файла"""
+    pass
+
+
+def get_last_5_operations(data):
+    """Получает последние 5 операций из списка"""
+    pass
