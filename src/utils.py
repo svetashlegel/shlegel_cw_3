4302d4826d619ee387dac24f49f6c95edaa98bac
shlegel <sveta.shlegel@mail.ru>
2023-04-30 20:54:40 +0300

    to the data loading function added formation of a list of instances of the Operations class, some functions updated to reflect changes in the Operations class

--- src/utils.py
+++ src/utils.py
@@ -4,28 +4,37 @@ from src import module
 
 
 def load_operations(path):
-    """Загружает список операций из файла"""
+    """Загружает операции из файла и создает список экземпляров класса Operations"""
+    operations_list = []
     with open(path, encoding="utf-8") as file:
         content = json.load(file)
-        return content
+    for op in content:
+        try:
+            operation = module.Operations(op["id"], op["state"], op["date"], op["description"], op["from"], op["to"],
+                                          op["operationAmount"]["amount"], op["operationAmount"]["currency"]["name"],
+                                          op["to"].split()[-1], op["from"].split()[-1])
+            operations_list.append(operation)
+        except KeyError:
+            operations_list.append(None)
+    return operations_list
 
 
-def get_last_5_operations(data):
+def get_last_five_operations(data):
     """Получает последние 5 операций из списка"""
     last_operations_list = []
     for operation in data:
-        if operation["state"] == "EXECUTED":
+        if operation.state == "EXECUTED":
             last_operations_list.append(operation)
         if len(last_operations_list) == 5:
             break
     return last_operations_list
 
 
-def get_right_format(raw_list):
-    """Создает список экземпляров класса Operations"""
-    right_format_list = []
-    for op in raw_list:
-        operation = module.Operations(op["date"], op["description"], op["from"], op["to"],
-                                      op["operationAmount"]["amount"], op["operationAmount"]["currency"]["name"])
-        right_format_list.append(operation)
-    return right_format_list
+def get_operation_data(operation):
+    """Возвращает форматированные данные по операции"""
+    hidden_card_num = operation.hide_card_num()
+    hidden_account_num = operation.hide_account_num()
+    return f"{operation.date.day}.{operation.date.month}.{operation.date.year} {operation.description}\n" \
+           f"{hidden_card_num} -> {hidden_account_num}\n" \
+           f"{operation.amount} {operation.currency}"
+
