7ebcd21c8b5f17e3ad7021867bbbcdc088175899
shlegel <sveta.shlegel@mail.ru>
2023-04-30 21:17:34 +0300

    path to the file is made into a constant

--- src/main_code.py
+++ src/main_code.py
@@ -1,7 +1,9 @@
 from utils import load_operations, get_last_five_operations, get_operation_data
 
+OPERATIONS_LIST_PATH = 'operations.json'
+
 # Загружаем данные из файла, формируем список экземпляров класса Operation
-operations_list = load_operations('operations.json')
+operations_list = load_operations(OPERATIONS_LIST_PATH)
 # Собираем последние 5 операций в список
 last_five_operations = get_last_five_operations(operations_list)
 # Формируем вывод данных по каждой операции
